f9abd9d12d0879ce6826a7d536abd6f4383d57aa
Nathan Marley <nathan.marley@gmail.com>
2015-05-13 12:20:39 -0500

    fixed and working

--- proc.rb
+++ proc.rb
@@ -1,29 +1,61 @@
 require 'nokogiri'
+require 'sanitize'
 require 'awesome_print'
 require 'pp'
 
 fn = 'index.html'
 html = File.read(fn)
 
-#ap html
-
-
-def strip_crap(html)
-  re = /(.*)<!--\ BEGIN\ PREHEADER\ \/\/\ -->(?:[^\n]*\n)
-        (.*)<!--\ \/\/\ END\ PREHEADER\ -->(?:[^\n]*\n)(.*)
+def strip_crap(html, thing)
+  re = /(.*)<!--\ BEGIN\ #{thing}\ \/\/\ -->(?:[^\n]*\n)
+        (.*)<!--\ \/\/\ END\ #{thing}\ -->(?:[^\n]*\n)(.*)
        /sxum
-        # (.*)END(.*)
-        # <!--\ END\ PRE/sxu
-  # re = /<!--\ BEGIN\ PREHEADER\ \/\/\ -->(?:[^\n]*\n)(.*?)<!--\ \/\/\ END\ PREHEADER\ -->\n/msxu
 
   new_html = html.sub(re) do |m|
-    puts $3
-    puts 'hi'
+    return $1 + $3
   end
 end
 
+html = strip_crap(html, 'PREHEADER')
+html = strip_crap(html, 'HEADER')
+html = strip_crap(html, 'FOOTER')
+# puts html
 
-strip_crap(html)
+doc = Nokogiri::HTML(html)
+doc.css('#awesomewrap').remove
+# ap doc.methods
+doc.css('script').remove                             # Remove <script>…</script>
+
+
+# doc.css('link[href="http://us5.campaign-archive2.com/css/archivebar-desktop.css"]').remove
+# link rel="stylesheet" href="http://us5.campaign-archive1.com/css/archivebar-desktop.css
+
+bad = []
+doc.css('link').each do |link|
+  href = link['href']
+  if href.match(/archivebar.*?css/)
+    bad.push( href )
+  end
+end
+
+bad.each do |href|
+  link_match = 'link[href="' + href + '"]'
+  doc.css(link_match).remove
+end
+
+# doc.css('link[href="http://us5.campaign-archive2.com/css/archivebar-desktop.css"]').remove
+# [href="http://us5.campaign-archive2.com/css/archivebar-desktop.css"]').remove
+
+doc.css('table.mcnShareBlock').remove
+doc.css('table.mcnDividerBlock').remove
+
+# TODO: change width attr from 600 to 900 where:...
+# <table class="mcnTextContentContainer"
+doc.css('table.mcnTextContentContainer')[0]['width'] = '900'
+
+html = doc.to_html
+
+puts html
 
 __END__
 <!-- BEGIN PREHEADER // -->
