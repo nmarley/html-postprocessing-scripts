ca0ef5ef651086def4c5eac077068c8bd251dc13
Nathan Marley <nathan.marley@gmail.com>
2015-05-18 10:11:14 -0500

    extract processing to a class

--- proc.rb
+++ proc.rb
@@ -1,66 +1,15 @@
-require 'nokogiri'
-require 'sanitize'
-require 'awesome_print'
-require 'pp'
+require './altucher'
+require 'httparty'
 
-fn = 'index.html'
-html = File.read(fn)
+# usage: echo 'mailchimp-email-url' | script > out.html
+# then view 'out.html' in browser ( echo file://`pwd`/out.html | pbcopy )
+# then adjust/remove extra elements and cmd-P
+# then save as PDF
+# then move to PC
+# and then print.
 
-def strip_crap(html, thing)
-  re = /(.*)<!--\ BEGIN\ #{thing}\ \/\/\ -->(?:[^\n]*\n)
-        (.*)<!--\ \/\/\ END\ #{thing}\ -->(?:[^\n]*\n)(.*)
-       /sxum
+url = ARGF.read
+resp = HTTParty.get(url)
+html = resp.body
+puts Altucher.transform(html)
 
-  new_html = html.sub(re) do |m|
-    return $1 + $3
-  end
-end
-
-html = strip_crap(html, 'PREHEADER')
-html = strip_crap(html, 'HEADER')
-html = strip_crap(html, 'FOOTER')
-# puts html
-
-doc = Nokogiri::HTML(html)
-doc.css('#awesomewrap').remove
-# ap doc.methods
-doc.css('script').remove                             # Remove <script>…</script>
-
-
-# doc.css('link[href="http://us5.campaign-archive2.com/css/archivebar-desktop.css"]').remove
-# link rel="stylesheet" href="http://us5.campaign-archive1.com/css/archivebar-desktop.css
-
-bad = []
-doc.css('link').each do |link|
-  href = link['href']
-  if href.match(/archivebar.*?css/)
-    bad.push( href )
-  end
-end
-
-bad.each do |href|
-  link_match = 'link[href="' + href + '"]'
-  doc.css(link_match).remove
-end
-
-# doc.css('link[href="http://us5.campaign-archive2.com/css/archivebar-desktop.css"]').remove
-# [href="http://us5.campaign-archive2.com/css/archivebar-desktop.css"]').remove
-
-doc.css('table.mcnShareBlock').remove
-doc.css('table.mcnDividerBlock').remove
-
-# TODO: change width attr from 600 to 900 where:...
-# <table class="mcnTextContentContainer"
-doc.css('table.mcnTextContentContainer')[0]['width'] = '900'
-
-html = doc.to_html
-
-puts html
-
-__END__
-<!-- BEGIN PREHEADER // -->
-<!-- // END PREHEADER -->
-<!-- BEGIN HEADER // -->
-<!-- // END HEADER -->
-<!-- BEGIN FOOTER // -->
-<!-- // END FOOTER -->
