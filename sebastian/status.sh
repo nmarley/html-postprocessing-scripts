7dad89791ab801dda81aef495b52a09a7f7f115d
Nathan Marley <nathan.marley@gmail.com>
2015-08-27 16:06:30 -0500

    update status

--- sebastian/status.sh
+++ sebastian/status.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-
-echo 'select slug, posted_at, length(orig_html) as size, fetched_at
+echo 'select slug, post_uid, posted_at, length(orig_html) as size, fetched_at
 from posts
--- where length(orig_html) > 0
-where length(orig_html) = 0
-order by posted_at desc
-limit 1
+where 1 = 1
+--  and length(orig_html) > 0
+--  and post_uid is null
+-- where length(orig_html) = 0
+order by posted_at
 \x\g\x' | psql -U sebastmarsh sebastmarsh_dev
-
 
