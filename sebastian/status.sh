8eb07a16241f44ddafab34d27ede673cff4ea722
Nathan Marley <nathan.marley@gmail.com>
2015-08-27 13:00:58 -0500

    filter status script

--- sebastian/status.sh
+++ sebastian/status.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
 
-echo 'select slug, posted_at, length(orig_html) as size, fetched_at from posts order by posted_at\x\g\x' | psql -U sebastmarsh sebastmarsh_dev
+echo 'select slug, posted_at, length(orig_html) as size, fetched_at
+from posts
+-- where length(orig_html) > 0
+where length(orig_html) = 0
+order by posted_at desc
+limit 1
+\x\g\x' | psql -U sebastmarsh sebastmarsh_dev
 
 
