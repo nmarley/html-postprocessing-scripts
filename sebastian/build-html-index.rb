70e85fc33a03ce5a93705ed854fc34e60817d8e6
Nathan Marley <nathan.marley@gmail.com>
2015-08-28 13:22:26 -0500

    fixup

--- sebastian/build-html-index.rb
+++ sebastian/build-html-index.rb
@@ -9,7 +9,6 @@ Sebastian::DB.connect
 Sebastian::Post.order(:posted_at).select(:posted_at, :slug, :post_uid).limit(7).each do |post|
   puts "<li>"
   ddate = post.posted_at.strftime("%Y-%m-%d %H%M")
-  #furl = "file://#{Sebastian::HTML_DIR}/#{post.html_filename}"
   furl = "file://#{post.filesystem_path}"
   atext = "#{ddate} - #{post.slug}"
   a = "<a href=\"#{furl}\">#{atext}</a>"
