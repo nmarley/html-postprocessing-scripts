3e8b275cb935ecd8bc9b64304f9d17700d7e97df
Nathan Marley <nathan.marley@gmail.com>
2015-08-28 22:57:27 -0500

    gen index for all pages

---
+++ sebastian/gendex.rb
@@ -0,0 +1,21 @@
+#! /usr/bin/env ruby
+
+require 'pp'
+require 'erb'
+
+Dir.chdir('site')
+entries = Dir.glob("page*")
+
+#Dir.chdir('/Users/nmarley/projects/altucher-proc/sebastian')
+
+@gendex = ""
+entries.sort_by { |ent| (ent.sub /^page/, '').to_i }.each do |p|
+  @gendex += "<a href=\"#{p}\">#{p}</a><br>\n"
+end
+
+fn = "index.html"
+File.open(fn, 'w') do |f|
+  erb = ERB.new(File.read('../gendex.html.erb'))
+  f.puts erb.result binding
+end
+
